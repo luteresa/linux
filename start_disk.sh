ce6d33641ad767f47828679a5fa2d9995f86bd08
Jason <luteresa@163.com>
2025-02-22 16:10:53 +0800

    for bpf
    Signed-off-by: Jason <luteresa@163.com>

--- start_disk.sh
+++ start_disk.sh
@@ -1,2 +1,6 @@
-qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine type=virt -m 1024 -smp 4 -kernel arch/arm64/boot/Image --append "noinitrd root=/dev/vda rw console=ttyAMA0 loglevel=8"  -nographic -drive if=none,file=rootfs_ext4.img,id=hd0 -device virtio-blk-device,drive=hd0 --fsdev local,id=kmod_dev,path=$PWD/kmodules,security_model=none -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount
+#!/bin/bash
 
+sudo ip tuntap add dev tap0 mode tap user $(whoami)
+sudo ip link set tap0 up
+sudo ip addr add 192.168.100.1/24 dev tap0
+qemu-system-aarch64  -nographic  -machine virt -cpu cortex-a57  -m 8192 -smp 8 -kernel arch/arm64/boot/Image   --append "noinitrd root=/dev/vda rw console=ttyAMA0 loglevel=8"  -drive if=none,file=rootfs.img,id=hd0 -device virtio-blk-device,drive=hd0 -device virtio-net-device,netdev=net0 -netdev tap,id=net0,ifname=tap0,script=no,downscript=no 
