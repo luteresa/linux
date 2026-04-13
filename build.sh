2f0a3bd62b5b94aba53a62bcc76c8132c3e8c531
hello <xx@gmail.com>
2026-04-13 08:05:14 +0000

    for perf and kprobe
    Signed-off-by: hello <xx@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-#make yos_defconfig
 OUTDIR='../out'
+make O=../out yos_defconfig
 #make O=../out ARCH=arm64 yos_defconfig
 #make O=../out ARCH=arm64 menuconfig
 #make O=../out ARCH=arm64 savedefconfig
